fd47097da2953ffaf0009ba3b05e0a44113cfc0c
KG <kimkiwonn@gmail.com>
2023-02-21 12:01:16 +0900

    fix: Car 이름 검증 추가

--- src/main/java/ch2/racing/Car.java
+++ src/main/java/ch2/racing/Car.java
@@ -5,7 +5,22 @@ public class Car {
     private int pos;
 
     public Car(String name) {
+        validName(name);
         this.name = name;
+    }
+
+    private void validName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Car 이름은 null일 수 없습니다.");
+        }
+
+        if (name == "") {
+            throw new IllegalArgumentException("Car 이름은 빈 문자열일 수 없습니다.");
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("Car 이름은 5자 초과일 수 없습니다.");
+        }
     }
 
     public int getPos() {
